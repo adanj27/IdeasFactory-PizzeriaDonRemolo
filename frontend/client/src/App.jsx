bc82b060a8e17f50df9249803b60a923942e0abb
A. J <117919468+adnj27@users.noreply.github.com>
2023-08-01 15:43:12 -0400

    fix: bug

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,12 +1,10 @@
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      <h1>Frontend</h1>
-    </>
-  )
+    <h1>Frontend</h1>
+  );
 }
 
-export default App
+export default App;
--- frontend/client/vite.config.js
+++ frontend/client/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'vite';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
