e1772bc260b404255a94cec04aa4ea1f8b792d1e
A. J <117919468+adnj27@users.noreply.github.com>
2023-08-20 09:15:16 -0400

    feat: :sparkles: add generic Card box, cards menu and Home page

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Footer from './components/footer/footer';
+import Home from './pages/Home';
 
 function App() {
   return (
 
     <div>
+      <Home />
       <h1>Frontend</h1>
       <Footer />
     </div>
---
+++ frontend/client/src/components/MenuCards.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Card from './Card';
+import { Menu, Locales, Contacto } from '../assets';
+
+function MenuCards() {
+  const cardData = [
+    {
+      img: Menu,
+      bg: 'bg-[#FF8333]',
+      title: 'MENÚ',
+      to: '/menu',
+    },
+    {
+      img: Locales,
+      bg: 'bg-[#BF60FF]',
+      title: 'LOCALES',
+      to: '/locales',
+    },
+    {
+      img: Contacto,
+      bg: 'bg-[#F25B72]',
+      title: 'CONTACTO',
+      to: '/contacto',
+    },
+  ];
+  return (
+    <section className="flex w-1/2 mx-auto my-12">
+      <div className="flex flex-wrap gap-10">
+        {cardData.map((item) => (
+          <div key={item.title}>
+            <Card img={item.img} bg={item.bg} title={item.title} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default MenuCards;
