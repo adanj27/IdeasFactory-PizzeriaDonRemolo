c603d9ea36bb2cbb867b25e173129f74b3890b9b
julianandrescaracas0623 <andrescaracas0623@gmail.com>
2023-08-20 15:04:00 -0500

    mejora de formato mas legible

--- frontend/client/src/components/Navbar/navbar.jsx
+++ frontend/client/src/components/Navbar/navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
-    // Aquí podrías realizar la lógica de búsqueda según el término ingresado
+    //Realizar la lógica de búsqueda según el término ingresado
   };
 
   const addToCart = () => {
@@ -23,7 +23,11 @@ function Navbar() {
       <div className="max-w-6xl mx-auto">
         <div className="flex flex-wrap justify-between items-center">
           <div className="w-full md:w-auto flex items-center mb-2 md:mb-0">
-            <img src="Dom romulo.png" alt="logo1" className="h-16 md:h-20 mx-auto md:mx-0" />
+            <img
+              src="Dom romulo.png"
+              alt="logo1"
+              className="h-16 md:h-20 mx-auto md:mx-0"
+            />
             <img src="pizza.png" alt="logo2" className="h-16" />
           </div>
 
@@ -39,6 +43,7 @@ function Navbar() {
             )}
 
             <button
+              type="button" 
               className="text-white hover:text-gray-300 p-1 bg-transparent focus:outline-none h-8"
               onClick={toggleSearch}
             >
@@ -51,10 +56,15 @@ function Navbar() {
 
             <div className="ml-3">
               <button
+                type="button" 
                 className="text-black p-1 bg-transparent focus:outline-none border border-none rounded shadow-md h-8 w-20 md:w-24 flex items-center justify-center"
                 onClick={addToCart}
               >
-                <img src="shopping_cart.png" alt="carrito" className="border-white mr-2 h-5" />
+                <img
+                  src="shopping_cart.png"
+                  alt="carrito"
+                  className="border-white mr-2 h-5"
+                />
                 <span>Carrito</span>
               </button>
             </div>
