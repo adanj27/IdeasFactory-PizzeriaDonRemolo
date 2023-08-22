37ae8d062c511f22bce48459731c9c07db1255bc
julianandrescaracas0623 <andrescaracas0623@gmail.com>
2023-08-21 20:26:15 -0500

    Revert "se Modifico de nuevo el Navbar ya que se habian perdido los cambios"
    This reverts commit c53a028a03f76a3a0b7f65e147bd8be1db7d8130.

--- frontend/client/src/components/Navbar/navbar.jsx
+++ frontend/client/src/components/Navbar/navbar.jsx
@@ -39,7 +39,6 @@ function Navbar() {
             )}
 
             <button
-              type="button"
               className="text-white hover:text-gray-300 p-1 bg-transparent focus:outline-none h-8"
               onClick={toggleSearch}
             >
@@ -52,7 +51,6 @@ function Navbar() {
 
             <div className="ml-3">
               <button
-                type="button"
                 className="text-black p-1 bg-transparent focus:outline-none border border-none rounded shadow-md h-8 w-20 md:w-24 flex items-center justify-center"
                 onClick={addToCart}
               >
