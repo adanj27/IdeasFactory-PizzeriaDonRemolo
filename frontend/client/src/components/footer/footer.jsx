0f557151910e5e18bafb4a06c1e4691dec15f070
julianandrescaracas0623 <andrescaracas0623@gmail.com>
2023-08-21 21:36:54 -0500

    componente footer modificado

--- frontend/client/src/components/footer/footer.jsx
+++ frontend/client/src/components/footer/footer.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg--200 bg-white py-8 text-center absolute w-full bottom-0">
-      <div className="text-black text-600">
-        <img src="Facebook.png" alt="logoFacebook" className="h-6 inline-block mr-4" />
-        <img src="Instagram.png" alt="logoInstagram" className="h-6 inline-block mr-4" />
-        <img src="Whatsapp.png" alt="logowhatsapp" className="h-6 inline-block" />
-        <img src="logo_4-removebg-preview 1.png" alt="logoPizza" className=" mx-auto" />
-        <p>Copyright © 2023 Pizzeria Don Remolo. All rights reserved.</p>
-      </div>
-    </footer>
+    <footer className="bg-white py-4 text-center absolute w-full bottom-0 mt-4">
+    <div className="container mx-auto">
+      <img src="Facebook.png" alt="logoFacebook" className="h-5 inline-block mr-4 -mb-3.5" />
+      <img src="Instagram.png" alt="logoInstagram" className="h-5 inline-block mr-4 -mb-3.5" />
+      <img src="Whatsapp.png" alt="logowhatsapp" className="h-5 inline-block -mb-3.5" />
+      <img src="logo_4-removebg-preview 1.png" alt="logoPizza" className="mx-auto py-auto h-18 -mb-5 " />
+      <p className="text-sm text-black py-auto -mb-1">Copyright © 2023 Pizzeria Don Remolo. All rights reserved.</p>
+    </div>
+    </footer>
   );
 }
 
