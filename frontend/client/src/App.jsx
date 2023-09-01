c1fd3c82f7f8a8b594e65342b7a8541c2834e524
AlexandraValdez <alexandravaldezs@hotmail.com>
2023-09-01 00:53:44 -0500

    router

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -3,14 +3,16 @@ import './App.css';
 import Footer from './components/footer/footer';
 import Navbar from './components/Navbar/navbar';// Asumiendo que este import es correcto
 import Home from './pages/Home';
+import Router from './routes/Router';
 
 function App() {
   return (
-    <div>
+    <>
+      <Router />
       <Navbar />
       <Home />
       <Footer />
-    </div>
+    </>
   );
 }
 
