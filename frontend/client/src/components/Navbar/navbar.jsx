02f45ea25a5984fcea83541bc259a6f407cd4b5a
julianandrescaracas0623 <andrescaracas0623@gmail.com>
2023-09-23 15:34:19 -0500

    Cambios realizados en Navbar

--- frontend/client/src/components/Navbar/navbar.jsx
+++ frontend/client/src/components/Navbar/navbar.jsx
@@ -1,77 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types'; // Importa PropTypes
 
 function Navbar() {
-  const [searchVisible, setSearchVisible] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [cartCount, setCartCount] = useState(0);
+  // ... (código restante)
+}
 
-  const toggleSearch = () => {
-    setSearchVisible(!searchVisible);
-  };
-
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const addToCart = () => {
-    setCartCount(cartCount + 1);
-  };
-
+function NavItem({ text }) {
   return (
-    <nav className="bg-white p-4">
-      <div className="max-w-6xl mx-auto">
-        <div className="flex flex-wrap justify-between items-center">
-          <div className="w-full md:w-auto flex items-center mb-2 md:mb-0">
-            <img
-              src="Dom romulo.png"
-              alt="logo1"
-              className="h-16 md:h-20 mx-auto md:mx-0"
-            />
-            <img src="pizza.png" alt="logo2" className="h-16" />
-          </div>
-
-          <div className="w-full md:w-auto flex items-center space-x-auto mt-2 md:mt-0">
-            {searchVisible && (
-              <input
-                type="text"
-                className="p-1 border-white shadow-md focus:outline-none w-full md:w-64"
-                placeholder="Buscar..."
-                value={searchTerm}
-                onChange={handleSearch}
-              />
-            )}
-
-            <button
-              type="button"
-              className="text-white hover:text-gray-300 p-1 bg-transparent focus:outline-none h-8"
-              onClick={toggleSearch}
-            >
-              <img
-                src="buscador.png"
-                alt="logo4"
-                className="shadow-gray-950 h-8 w-auto"
-              />
-            </button>
-
-            <div className="ml-3">
-              <button
-                type="button"
-                className="text-black p-1 bg-transparent focus:outline-none border border-none rounded shadow-md h-8 w-20 md:w-24 flex items-center justify-center"
-                onClick={addToCart}
-              >
-                <img
-                  src="shopping_cart.png"
-                  alt="carrito"
-                  className="border-white mr-2 h-5"
-                />
-                <span>Carrito</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <button
+      type="button"
+      className="h-7 w-24 text-black border-none hover:bg-orange-500 rounded-lg"
+    >
+      {text}
+    </button>
   );
 }
+
+// Agrega la validación de propiedades para NavItem
+NavItem.propTypes = {
+  text: PropTypes.string.isRequired, // Valida que 'text' sea una cadena y sea requerido
+};
+
+function CartButton() {
+  return (
+    <button
+      type="button"
+      className="h-7 w-24 text-black border-none hover:bg-orange-500 rounded-lg flex items-center"
+    >
+      Carrito
+      <img
+        src="carrito.png"
+        alt="carrito"
+        className="h-4 ml-1"
+      />
+    </button>
+  );
+}
+
+// Agrega la validación de propiedades para CartButton de la misma manera que NavItem
+CartButton.propTypes = {
+  // Si es necesario, agrega las props y validaciones necesarias aquí
+};
 
 export default Navbar;
