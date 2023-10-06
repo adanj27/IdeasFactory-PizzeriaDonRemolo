6cb204e03c02711e3bf7c2113935d40fbb367b55
Yf3rs0n <yferson5020@gmail.com>
2023-10-06 18:36:44 -0500

    subtle changes to http responses and code review - Yeferson

--- backend/src/controllers/CategoryController.php
+++ backend/src/controllers/CategoryController.php
@@ -5,38 +5,43 @@ namespace App\Controllers;
 use App\Config\ResponseHttp;
 use App\Models\CategoryModel;
 
-class CategoryController extends BaseController{
+class CategoryController extends BaseController {
     
     /**
      * Create a new category
      */
-    public function createCategory(){
-        // verify if the request is PUT
-        if ($this->isGetMethod() && $this->getRoute() === 'category/create') {
-            // gets the parameters of the request
+    public function createCategory() {
+        // Verify if the request is POST
+        if ($this->isPostMethod() && $this->getRoute() === 'category/create') {
+            // Get the parameters of the request
             $param = $this->getParam();
-            // check if you have all the required fields 
-            if (array_key_exists('name_category', $param)){
+            
+            // Check if you have all the required fields 
+            if (array_key_exists('name_category', $param)) {
                 $CategoryModel = new CategoryModel([
                     'name_category' => $param['name_category']
-                    ]);
+                ]);
                 $result = $CategoryModel->create($param);
-                echo json_encode($result);
+                
+                // Check the result and send an appropriate response
+                if ($result) {
+                    echo json_encode(ResponseHttp::status201('Category created successfully'));
+                } else {
+                    echo json_encode(ResponseHttp::status500('Failed to create category'));
+                }
                 exit;
             } else {
-                echo json_encode(['error' => 'Required data missing']);
+                echo json_encode(ResponseHttp::status400('Required data missing'));
                 exit;
             }
         }
     }
 
-
-
     /**
      * Query category by ID
      */
-    public function getCategoryById($id){
-        // checks if the request is GET and if the path matches "category/id".
+    public function getCategoryById($id) {
+        // Check if the request is GET and if the path matches "category/id".
         if ($this->isGetMethod() && $this->getRoute() === "category/$id") {
             $CategoryModel = new CategoryModel([]);
             $result = $CategoryModel->getById($id);
@@ -46,60 +51,66 @@ class CategoryController extends BaseController{
     }
 
     /**
-     * get all the category 
+     * Get all categories
      */
-    final public function getAll(string $endPoint){
-        // check if the request is GET
-        if ($this->isGetMethod() == 'get' && $endPoint == $this->getRoute()) {
+    final public function getAll(string $endPoint) {
+        // Check if the request is GET
+        if ($this->isGetMethod() && $endPoint == $this->getRoute()) {
             // Create an instance of CategoryModel
             $CategoryModel = new CategoryModel([]);
             // Call the all() method of the instance created to obtain the categories.
-            $Category = $CategoryModel->all();
-            echo json_encode($Category);
+            $categories = $CategoryModel->all();
+            echo json_encode($categories);
             exit;
         }
     }
 
-
     /**
-     * update category
+     * Update category
      * @parameters : $id
      */
-    public function updateCategory($id){
-        // verify if the request is PUT
-        if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+    public function updateCategory($id) {
+        // Verify if the request is PUT
+        if ($this->isPutMethod() && $this->getRoute() === "category/$id") {
             $param = $this->getParam();
-            // check if you have all the required fields 
-            if (array_key_exists('name_category', $param)){
+            // Check if you have all the required fields 
+            if (array_key_exists('name_category', $param)) {
                 $CategoryModel = new CategoryModel([
-                        'name_category' => $param['name_category']
-                    ]);
+                    'name_category' => $param['name_category']
+                ]);
                 $result = $CategoryModel->update($id, $param);
-                echo json_encode($result);
+                
+                // Check the result and send an appropriate response
+                if ($result) {
+                    echo json_encode(ResponseHttp::status200('Category updated successfully'));
+                } else {
+                    echo json_encode(ResponseHttp::status500('Failed to update category'));
+                }
                 exit;
             } else {
-                echo json_encode(['error' => 'Required data missing']);
+                echo json_encode(ResponseHttp::status400('Required data missing'));
                 exit;
             }
         }
     }
 
-
-
     /**
-     * delete category
+     * Delete category
      * @parameters : $id
      */
-    public function deleteCategory($id){
-       // verify if the request is DELETE
-        if ($this->isGetMethod() && $this->getRoute() === "category/$id") {
-           $CategoryModel = new CategoryModel([]);
-           $result = $CategoryModel->delete($id);
-            echo json_encode($result);
+    public function deleteCategory($id) {
+        // Verify if the request is DELETE
+        if ($this->isDeleteMethod() && $this->getRoute() === "category/$id") {
+            $CategoryModel = new CategoryModel([]);
+            $result = $CategoryModel->delete($id);
+            
+            // Check the result and send an appropriate response
+            if ($result) {
+                echo json_encode(ResponseHttp::status200('Category deleted successfully'));
+            } else {
+                echo json_encode(ResponseHttp::status500('Failed to delete category'));
+            }
             exit;
         }
     }
-
-
-}    
-    
+}
