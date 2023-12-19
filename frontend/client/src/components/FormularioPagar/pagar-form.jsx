965e8e75be06cffd70a9ad294f4f226e2de258fb
ReloJunior <andresrodriguezab@hotmail.com>
2023-12-19 19:40:50 -0300

    Redireccion al wsp y mensaje en la pagina de agradecimiento

--- frontend/client/src/components/FormularioPagar/pagar-form.jsx
+++ frontend/client/src/components/FormularioPagar/pagar-form.jsx
@@ -1,18 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormContext } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 import Input from './Input';
-
-const handlePedidoClick = () => {
-  // Número de teléfono y mensaje predefinido
-  const phoneNumber = '+542214345359'; // Reemplaza con el número de teléfono al que quieres enviar el mensaje
-  const mensaje = '¡Hola! Quiero realizar un pedido.';
-
-  // Construir el enlace de WhatsApp
-  const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(mensaje)}`;
-
-  // Redirigir a la ventana de WhatsApp
-  window.location.href = whatsappLink;
-};
 
 const nameRules = {
   required: 'Debe ingresar un nombre completo',
@@ -40,27 +29,61 @@ const dirRules = {
 };
 
 function PagarForm() {
+  const [hidden, setHidden] = useState(false);
   const { handleSubmit } = useFormContext();
 
-  return (
-    <div className="max-w-md mx-auto my-36 relative overflow-hidden z-10 bg-white p-8 rounded-lg shadow-md border-2 border-[#CF5100]">
-      <h2 className="text-2xl text-[#CF5100] font-bold mb-6">Completá tus datos:</h2>
+  const onSubmit = () => {
+    // Número de teléfono y mensaje predefinido
+    const phoneNumber = '+542214345359'; // Reemplaza con el número de teléfono al que quieres enviar el mensaje
+    const mensaje = '¡Hola Pizzería Deliciosa!';
 
-      <form onSubmit={handleSubmit} noValidate autoComplete="off">
-        <Input name="nombre" type="text" placeholder="Ingresá tu nombre..." rules={nameRules} />
-        <Input name="phone" type="phone" placeholder="221-1234567" rules={phoneRules} />
-        <Input name="dir" type="text" placeholder="Ingresá tu dirección..." rules={dirRules} />
-        <div className="flex justify-end">
-          <button
-            onClick={handlePedidoClick}
-            className="bg-[#CF5100] text-white px-4 py-2 font-bold rounded-md hover:opacity-80"
-            type="submit"
-          >
-            Pedir
+    /*
+`
+Nuevo pedido de ChatGPT:
+
+Nombre: ${nombre}
+Dirección: ${direccion}
+Pedido: ${pedido}
+Monto Total: $${montoTotal.toFixed(2)}
+
+¡Gracias!
+`; */
+
+    // Construir el enlace de WhatsApp
+    const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(mensaje)}`;
+
+    // Redirigir a la ventana de WhatsApp
+    window.open(whatsappLink, '_blank');
+    setHidden(true);
+  };
+
+  return (
+    hidden ? (
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-4xl text-center mt-36 font-display text-[#CF5100]">Gracias por tu pedido!</h2>
+        <p className="text-xl text-center py-8 font-display"> Puede seguir su pedido a traves del chat de WhatsApp </p>
+        <Link to="/">
+          <button className="bg-[#CF5100] text-white px-4 py-2 font-semibold rounded-md hover:opacity-80" type="submit">
+            Volver al inicio
           </button>
-        </div>
-      </form>
-    </div>
+        </Link>
+      </div>
+    ) : (
+      <div className="max-w-md mx-auto my-36 relative overflow-hidden z-10 bg-white p-8 rounded-lg shadow-md border-2 border-[#CF5100]">
+        <h2 className="text-2xl text-[#CF5100] font-bold mb-6">Completá tus datos:</h2>
+
+        <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off">
+          <Input name="nombre" type="text" placeholder="Ingresá tu nombre..." rules={nameRules} />
+          <Input name="phone" type="phone" placeholder="221-1234567" rules={phoneRules} />
+          <Input name="dir" type="text" placeholder="Ingresá tu dirección..." rules={dirRules} />
+          <div className="flex justify-end">
+            <button className="bg-[#CF5100] text-white px-4 py-2 font-bold rounded-md hover:opacity-80" type="submit">
+              Pedir
+            </button>
+          </div>
+        </form>
+      </div>
+    )
   );
 }
 
